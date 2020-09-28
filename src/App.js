8e6ea6f68a69b817e299038ff15ffb725e39e2df
jethrosama <kel73896@gmail.com>
2020-09-28 22:21:27 +0800

    fetch countries from api & finish map countries dropdown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const BASE_API = 'https://disease.sh/v3/covid-19';
+  console.log('re render');
+  const [countries, setCountries] = useState([]);
+  const [dropdownValue, setDropdownValue] = useState('worldwide');
+  const handleDropdown = (evt) => {
+    evt.preventDefault();
+    setDropdownValue(evt.target.value);
+  };
+  useEffect(() => {
+    const FetchCountries = async () => {
+      console.log('useeffect');
+      let response = await axios.get(`${BASE_API}/countries`);
+      let countries = response.data.map((country) => ({
+        name: country.country,
+        value: country.countryInfo.iso2,
+      }));
+      setCountries(countries);
+    };
+    FetchCountries();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__header'>
+        <h1>COVID_19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+          <Select
+            variant='outlined'
+            value={dropdownValue}
+            onChange={handleDropdown}
+          >
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem key={country.name} value={country.name}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* header */}
+      {/* title & select box */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Map */}
+
+      {/* table */}
+      {/* graph */}
     </div>
   );
 }
