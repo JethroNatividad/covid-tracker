c395455de1419c046cc42a5df8a8599929fa3030
jethrosama <kel73896@gmail.com>
2020-10-03 21:36:14 +0800

    add comment to Map and InfoBox

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,27 +1,38 @@
+import React from 'react';
+import { actions } from './reducer';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import { capitalizeFirstLetter, formatNumber } from './helpers';
-import React from 'react';
-import './InfoBox.css';
 import { useCountriesContext, useCountriesDispatch } from './CountriesProvider';
-import { actions } from './reducer';
+import './InfoBox.css';
 function InfoBox({ title, caseType }) {
-  console.log('in InfoBox');
+  //context data
   const { countryInfo, casesType } = useCountriesContext();
   const { dispatch } = useCountriesDispatch();
+
+  //this checks the current selected infobox, returns true if it is
   const active = caseType === casesType;
+
+  //this returns the total cases of a case type from the data,
   const total = countryInfo[caseType];
+
+  //this returns the number of todays cases of a case type
   const cases = countryInfo[`today${capitalizeFirstLetter(caseType)}`];
+
+  //this dictionary contains the color of each casetype
   const color = {
     cases: ' #CC1034',
     recovered: ' #7dd71d',
     deaths: ' #fb4443',
   };
+
   const handleClick = () => {
+    //this sets the case type to the current case type if clicked
     dispatch({ type: actions.SET_CASES_TYPE, data: caseType });
   };
   return (
     <Card
       className='infoBox'
+      /* here checks if the box is active, adds bordertop if true */
       style={{ borderTop: active && `10px solid ${color[caseType]}` }}
       onClick={handleClick}
     >
--- src/Map.js
+++ src/Map.js
@@ -4,15 +4,19 @@ import { useCountriesContext } from './CountriesProvider.js';
 import { showDataOnMap } from './helpers.js';
 import './Map.css';
 function Map() {
-  console.log('in Map');
+  //context
   const { mapCountries, mapCenter, mapZoom, casesType } = useCountriesContext();
   return (
     <div className='map'>
+      {/* LeafMap accepts center and zoom, center accepts lat and lng, zoom accepts number */}
       <LeafletMap center={mapCenter} zoom={mapZoom}>
+        {/* These attributes are given, copied from the docs */}
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {/* this is generates the circles, the mapcountries is the data response on request ${api_url}/all */}
+        {/* casesType allow to change if viewing cases, deaths and recovered */}
         {showDataOnMap(mapCountries, casesType)}
       </LeafletMap>
     </div>
